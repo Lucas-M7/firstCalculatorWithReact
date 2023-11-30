ea8affbdee2c4cfd6d4fd10fabef0499d11a3561
Lucas-M7 <Lucas015x@gmail.com>
2023-11-30 14:44:24 -0300

    Upload

---
+++ src/components/input/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAF;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+    font-size: 24px;
+    font-family: 'Robot', sans-serif;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFF;
+        border: 0;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Robot', sans-serif;
+        color: #FFFF;
+    }
+`
